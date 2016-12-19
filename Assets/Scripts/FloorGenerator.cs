0c51b62948fca4c7d0a3ee921684c3ab9b9d5966
pgod <p.godziszewski@o2.pl>
2016-12-19 18:32:33 +0100

    Added simple floor generation

---
+++ Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorGenerator : MonoBehaviour {
+    public GameObject prefab;
+    private float distanceForNewCube = 0F;
+    private System.Random random;
+    private float startAfterSeconds = 1.0f;
+    private float timeBetweenInvocations = 0.3f;
+    private bool justCreatedCube;
+    // Use this for initialization
+    void Start () {
+        this.random = new System.Random();
+        InvokeRepeating("GenerateCube", startAfterSeconds, timeBetweenInvocations);
+    }
+
+    void GenerateCube()
+    {
+        if(FloorShouldBeCreated())
+        {
+            GenerateFloorCube();
+            justCreatedCube = true;
+        }
+        else
+        {
+            justCreatedCube = false;
+        }
+        distanceForNewCube++;
+
+    }
+    bool FloorShouldBeCreated()
+    {
+        return (!justCreatedCube || random.Next(2, 5) % 2  == 0);
+    }
+    void GenerateFloorCube()
+    {
+        GameObject cube = (GameObject)Instantiate(prefab);
+        cube.transform.position = new Vector3(4.533F + distanceForNewCube, -1.95F, 10);
+    }
+}
