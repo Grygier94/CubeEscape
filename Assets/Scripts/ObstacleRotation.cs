228854884026426f71b84d7e662ea74af3ddcb02
Marcin Grygierczyk <kwaska1000@gmail.com>
2016-12-22 08:31:41 +0100

    Obstacle added

---
+++ Assets/Scripts/ObstacleRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleRotation : MonoBehaviour {
+	
+	void Update () {
+        transform.Rotate(Vector3.forward * Time.timeScale * 3f);
+	}
+}
