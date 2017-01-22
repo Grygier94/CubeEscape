6871d4cad45beedb215015556a2f3c458d2399f6
Marcin Grygierczyk <kwaska1000@gmail.com>
2017-01-22 16:16:20 +0100

    Speaker animation works fine on every scene now

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -8,17 +8,26 @@ public class SoundManager : MonoBehaviour {
     public Sprite soundOff;
     bool isMute;
 
+    void Start()
+    {
+
+        if (PlayerPrefs.GetInt("isMute") == 1)
+            Mute();
+    }
+
     public void Mute()
     {
         isMute = !isMute;
 
         if(isMute)
         {
+            PlayerPrefs.SetInt("isMute", 1);
             AudioListener.volume = 0;
             GetComponent<Image>().sprite = soundOff;
         }
         else
         {
+            PlayerPrefs.SetInt("isMute", 0);
             AudioListener.volume = 1;
             GetComponent<Image>().sprite = soundOn;
         }
